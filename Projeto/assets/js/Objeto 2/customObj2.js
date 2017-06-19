d64e02347cd5cc8ed701f8e99ae1f3a543dc0078
Antonio Carlos Catani Neto <cataninetoo@gmail.com>
2017-06-19 17:26:38 -0300

    [Objeto 2] Adicionados elementos básicos para a tela de nome

--- Projeto/assets/js/Objeto 2/customObj2.js
+++ Projeto/assets/js/Objeto 2/customObj2.js
@@ -1,8 +1,53 @@
+var scene;
+
+
 $(document).ready(function() {
 	hideDivsOnObjectStart();
-	});
+});
 
 
 function hideDivsOnObjectStart() {
 	$("#calculadora").hide();
+	$("#iconesFixos").hide();
+	hideIconsNome();
 }
+
+function hideIconsNome() {
+	$("#alertNome").hide();
+}
+
+function nextScene() {
+	unloadScene();
+	scene++;
+	loadScene();
+
+}
+
+function previousScene() {
+	unloadScene();
+	scene();
+	loadScene();
+}
+
+function loadScene() {
+
+	switch (scene) {
+		case 1:
+		break;
+	}
+}
+
+function unloadScene() {
+	switch (scene) {
+		case 1:
+		break;
+	}
+}
+
+$( "#verificacaoNome" ).keypress(function( event ) {
+	alert("oi");
+
+	if ( event.which == 13 ) {
+		alert("oi");
+	}
+});
