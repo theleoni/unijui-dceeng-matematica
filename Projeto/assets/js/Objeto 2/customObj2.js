96462e9139e6d5e51a5e84fb1dc0a2d16286d547
Antonio Carlos Catani Neto <cataninetoo@gmail.com>
2017-06-23 14:36:58 -0300

    [Objeto 2] Todos os diálogos da tela Inicial adicionados [Objeto 2] Adicionada navegação (Passar e voltar) nos diálogos da Tela inicial [Objeto 2] Adicionado aviso caso o usuário não passe para a segunda tela dentro de 5 segundos

--- Projeto/assets/js/Objeto 2/customObj2.js
+++ Projeto/assets/js/Objeto 2/customObj2.js
@@ -1,10 +1,12 @@
 var scene;
 
 var defaultFadeTime = 500;
-var extendedFadeTime = 3000;
-
+var extendedFadeTime = 1250;
+var shortFadeTime = 250;
 var nomeDoUsuario;
 
+var scene1DialogControl = 0;
+var stuckOnScene1 = true;
 
 $(document).ready(function() {
 	hideDivsOnObjectStart();
@@ -73,12 +75,208 @@ $(document).on('click', '#botaoNome', function() {
 		nomeDoUsuario = $("#inputNome").val();
 		$("#inputNomeContainer").fadeOut(defaultFadeTime);
 		$("#containerPersonagemExtraTelaNome").fadeIn(extendedFadeTime);
+		$("#balao1TelaNome").show();
+		$("#balao1TelaNome").css("opacity", "0");
 		$("#balao2TelaNome").fadeIn(extendedFadeTime*1.5);
 		$("#fala2TelaNome").html(dataJSON.telaNome.falaChico1);
 		$("#fala2TelaNome").fadeIn(extendedFadeTime*1.5);
-		$("#setaEsquerdaNome").fadeIn(defaultFadeTime);
 		$("#setaDireitaNome").fadeIn(defaultFadeTime)
 		$("#alertNome").show();
+		disallowNextScene();
+		$("#iconSetaEsquerda").hide();
 	}
 });
 
+
+$(document).on('click', '#setaDireitaNome', function() {
+	switch (scene1DialogControl) {
+		case 0: 
+		$("#setaEsquerdaNome").show();
+		$("#fala2TelaNome").html(dataJSON.telaNome.falaChico2);
+		scene1DialogControl++;
+		break;
+
+		case 1:
+		$("#balao1TelaNome").css("opacity", "1");
+		$("#fala1TelaNome").html(dataJSON.telaNome.falaCrianças1);
+		$("#fala1TelaNome").fadeIn(shortFadeTime);
+		$("#balao2TelaNome").css("opacity", "0");
+		$("#fala2TelaNome").html("");
+		$("#fala2TelaNome").fadeOut(shortFadeTime);
+		scene1DialogControl++;
+		break;
+
+		case 2: 
+		$("#balao2TelaNome").css("opacity", "1");
+		$("#fala2TelaNome").html(dataJSON.telaNome.falaChico3);
+		$("#fala2TelaNome").fadeIn(shortFadeTime);
+		$("#balao1TelaNome").css("opacity", "0");
+		$("#fala1TelaNome").html("");
+		$("#fala1TelaNome").fadeOut(shortFadeTime);
+		scene1DialogControl++;
+		break;
+
+		case 3:
+		$("#balao1TelaNome").css("opacity", "1");
+		$("#fala1TelaNome").html(dataJSON.telaNome.falaCrianças2);
+		$("#fala1TelaNome").fadeIn(shortFadeTime);
+		$("#balao2TelaNome").css("opacity", "0");
+		$("#fala2TelaNome").html("");
+		$("#fala2TelaNome").fadeOut(shortFadeTime);
+		scene1DialogControl++;
+		break;
+
+		case 4:
+		$("#balao2TelaNome").css("opacity", "1");
+		$("#fala2TelaNome").html(dataJSON.telaNome.falaChico4);
+		$("#fala2TelaNome").fadeIn(shortFadeTime);
+		$("#balao1TelaNome").css("opacity", "0");
+		$("#fala1TelaNome").html("");
+		$("#fala1TelaNome").fadeOut(shortFadeTime);
+		scene1DialogControl++;
+		break;
+
+		case 5:
+		$("#balao1TelaNome").css("opacity", "1");
+		$("#fala1TelaNome").html(dataJSON.telaNome.falaCrianças3);
+		$("#fala1TelaNome").fadeIn(shortFadeTime);
+		$("#balao2TelaNome").css("opacity", "0");
+		$("#fala2TelaNome").html("");
+		$("#fala2TelaNome").fadeOut(shortFadeTime);
+		scene1DialogControl++;
+		break;
+
+		case 6:
+		$("#balao2TelaNome").css("opacity", "1");
+		$("#fala2TelaNome").html(dataJSON.telaNome.falaChico5);
+		$("#fala2TelaNome").fadeIn(shortFadeTime);
+		$("#balao1TelaNome").css("opacity", "0");
+		$("#fala1TelaNome").html("");
+		$("#fala1TelaNome").fadeOut(shortFadeTime);
+		scene1DialogControl++;
+		break;
+
+		case 7:
+		$("#balao1TelaNome").css("opacity", "1");
+		$("#fala1TelaNome").html(dataJSON.telaNome.falaCrianças4);
+		$("#fala1TelaNome").fadeIn(shortFadeTime);
+		$("#balao2TelaNome").css("opacity", "0");
+		$("#fala2TelaNome").html("");
+		$("#fala2TelaNome").fadeOut(shortFadeTime);
+		scene1DialogControl++;
+		allowNextScene();
+		$("#setaDireitaNome").hide();
+
+		setTimeout(function() {
+			if(stuckOnScene1) {
+				swal("Preso aqui?", "Clique na seta no canto inferior direito para prosseguir");
+				stuckOnScene1 = false;
+			}
+		}, 5000) 
+
+		break;
+
+	}
+});
+
+$(document).on('click', '#setaEsquerdaNome', function() {
+	switch (scene1DialogControl) {
+
+		case 1:
+		$("#fala2TelaNome").html(dataJSON.telaNome.falaChico1);
+		scene1DialogControl--;
+		$("#setaEsquerdaNome").hide();
+		break;
+
+		case 2: 
+		$("#balao2TelaNome").css("opacity", "1");
+		$("#fala2TelaNome").html(dataJSON.telaNome.falaChico2);
+		$("#fala2TelaNome").fadeIn(shortFadeTime);
+		$("#balao1TelaNome").css("opacity", "0");
+		$("#fala1TelaNome").html("");
+		$("#fala1TelaNome").fadeOut(shortFadeTime);
+		scene1DialogControl--;
+		break;
+
+		case 3:
+		$("#balao1TelaNome").css("opacity", "1");
+		$("#fala1TelaNome").html(dataJSON.telaNome.falaCrianças1);
+		$("#fala1TelaNome").fadeIn(shortFadeTime);
+		$("#balao2TelaNome").css("opacity", "0");
+		$("#fala2TelaNome").html("");
+		$("#fala2TelaNome").fadeOut(shortFadeTime);
+		scene1DialogControl--;
+		break;
+
+		case 4:
+		$("#balao2TelaNome").css("opacity", "1");
+		$("#fala2TelaNome").html(dataJSON.telaNome.falaChico3);
+		$("#fala2TelaNome").fadeIn(shortFadeTime);
+		$("#balao1TelaNome").css("opacity", "0");
+		$("#fala1TelaNome").html("");
+		$("#fala1TelaNome").fadeOut(shortFadeTime);
+		scene1DialogControl--;
+		break;
+
+		case 5:
+		$("#balao1TelaNome").css("opacity", "1");
+		$("#fala1TelaNome").html(dataJSON.telaNome.falaCrianças2);
+		$("#fala1TelaNome").fadeIn(shortFadeTime);
+		$("#balao2TelaNome").css("opacity", "0");
+		$("#fala2TelaNome").html("");
+		$("#fala2TelaNome").fadeOut(shortFadeTime);
+		scene1DialogControl--;
+		break;
+
+		case 6:
+		$("#balao2TelaNome").css("opacity", "1");
+		$("#fala2TelaNome").html(dataJSON.telaNome.falaChico4);
+		$("#fala2TelaNome").fadeIn(shortFadeTime);
+		$("#balao1TelaNome").css("opacity", "0");
+		$("#fala1TelaNome").html("");
+		$("#fala1TelaNome").fadeOut(shortFadeTime);
+		scene1DialogControl--;
+		break;
+
+		case 7:
+		$("#balao1TelaNome").css("opacity", "1");
+		$("#fala1TelaNome").html(dataJSON.telaNome.falaCrianças3);
+		$("#fala1TelaNome").fadeIn(shortFadeTime);
+		$("#balao2TelaNome").css("opacity", "0");
+		$("#fala2TelaNome").html("");
+		$("#fala2TelaNome").fadeOut(shortFadeTime);
+		scene1DialogControl--;
+		break;
+
+		case 8:
+		$("#balao2TelaNome").css("opacity", "1");
+		$("#fala2TelaNome").html(dataJSON.telaNome.falaChico5);
+		$("#fala2TelaNome").fadeIn(shortFadeTime);
+		$("#balao1TelaNome").css("opacity", "0");
+		$("#fala1TelaNome").html("");
+		$("#fala1TelaNome").fadeOut(shortFadeTime);
+		scene1DialogControl--;
+		$("#setaDireitaNome").show();
+		break;
+
+	}
+});
+
+
+$(document).on('click', '#iconSetaDireita', function() {
+	switch (scene) {
+		case 1:
+		stuckOnScene1 = false;
+		iconSetaEsquerda.show();
+		nextScene();
+		break;
+	}
+});
+
+function allowNextScene() {
+	$("#iconSetaDireita").show();
+}
+
+function disallowNextScene() {
+	$("#iconSetaDireita").hide();
+}
