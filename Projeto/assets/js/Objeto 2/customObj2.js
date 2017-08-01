edd787d5643c4183f6c96a7241f29414750fb1fc
Antonio Carlos Catani Neto <cataninetoo@gmail.com>
2017-08-01 14:52:43 -0300

    [Objeto 2] Correção na cor do alerta da atividade 1

--- Projeto/assets/js/Objeto 2/customObj2.js
+++ Projeto/assets/js/Objeto 2/customObj2.js
@@ -1318,11 +1318,15 @@ function loadQuestion() {
 				if (numeroErradasModa == 0) {
 					$("#alertAtividade1").html(dataJSON.atividadeLivro1.msgCorreta);
 					$("#alertAtividade1").show();
+					$("#alertQ1").addClass("alert-success");
+					$("#alertQ1").removeClass("alert-danger");
 					$(".hideColumn1Ativ1").css("display", "table-cell");
 					fixarValoresModa();
 				} else {
 					$("#alertAtividade1").show();
 					$("#alertAtividade1").html(dataJSON.atividadeLivro1.msgErrada.replace("%mesesErrados%", mesesErradosModa));
+					$("#alertQ1").addClass("alert-danger");
+					$("#alertQ1").removeClass("alert-success");
 				}
 
 			} else if (verificacaoAtividade1[1] == false) {
