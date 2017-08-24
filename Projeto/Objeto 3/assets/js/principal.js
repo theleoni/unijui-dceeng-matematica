2d35c473104bb73aec9a6de91cbe462fb5393403
Guilherme Leoni <guilherme_leoni@outlook.com>
2017-08-24 16:44:57 -0300

    [Objeto 3] Alteração de teste para o Android
    Objetivo de atualizar os videos para que sejam automaticamente executados no Android

--- Projeto/Objeto 3/assets/js/principal.js
+++ Projeto/Objeto 3/assets/js/principal.js
@@ -3,9 +3,14 @@ var loginVideo = document.querySelector('.scr-login video');
 var source = document.createElement('source');
 source.setAttribute('src', 'assets/video/login-1.mp4');
 loginVideo.appendChild(source);
-loginVideo.play();
+// loginVideo.play()
+
+loginVideo.addEventListener('canplay', function() {
+	loginVideo.play();
+});
 
 let loginButtonConfirmacao = document.querySelector("#login-button-confirmacao");
+console.log(loginButtonConfirmacao);
 loginButtonConfirmacao.addEventListener('click', function() {
 	loginVideo.addEventListener('ended', function myHandler(e) {
 
