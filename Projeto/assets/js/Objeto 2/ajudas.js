ecda9b10e9406de7bc562b1f6651839696c31d39
Antonio Carlos Catani Neto <cataninetoo@gmail.com>
2017-08-29 16:15:24 -0300

    [Objeto 2] Adicionadas "steps" à ajuda da tela 8

--- Projeto/assets/js/Objeto 2/ajudas.js
+++ Projeto/assets/js/Objeto 2/ajudas.js
@@ -14,11 +14,13 @@ var ajudaTela8 = [
   html: 'Na <strong>Média Aritmética Ponderada</strong> existem casos onde as ocorrências têm importância relativa diferentes. Nestes, o cálculo da média deve levar em conta esta importância relativa ou peso relativo.<br>Ponderar é sinônimo de pesar. No cálculo da média ponderada, multiplicamos cada valor do conjunto por seu "peso", isto é, sua importância relativa.'
 },
 {
+  progressSteps: ['1', '2', '3'],
   title: 'Ajuda',
   confirmButtonText: 'Próxima &rarr;',
   html: '<table id="tabelaAgrupadaConsumoMensal" class="table table-responsive table-bordered"><thead><tr><td><strong>Consumo diário em kWh</strong></td><td><strong>Nº de dias de consumo</strong></td> </tr> </thead><tr><td class="noLeftBorder">3,0</td><td class="noRightBorder">2</td></tr><tr><td class="noLeftBorder">3,5</td><td class="noRightBorder">4</td></tr> <tr><td class="noLeftBorder">3,8</td><td class="noRightBorder"> 5</td></tr><tr> <td class="noLeftBorder">4,0</td><td class="noRightBorder">17</td> </tr><tr> <td class="noLeftBorder tabelaTotal">Soma</td> <td class="noRightBorder tabelaTotal">28</td></tr> </table> <br> Para encontrar a média aritmética do consumo diário, podemos utilizar o conceito da média ponderada utilizando o número de dias como peso, pois cada valor se repetiu durante certo número de dias.'
 },
 {
+  progressSteps: ['1', '2', '3'],
   title: 'Ajuda',
   confirmButtonText: 'Próxima &rarr;',
   html: 'Veja como calcular a média do consumo diário. Use uma calculadora para obter o resultado <br><img src="../assets/img/Objeto 2/equacaoTela8.png">'
