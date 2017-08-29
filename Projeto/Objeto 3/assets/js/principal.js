bf9711e4265c8cfceae98a2b51c121d410c95f20
Guilherme Leoni <guilherme_leoni@outlook.com>
2017-08-29 15:50:39 -0300

    Configuração da Tela de Login

--- Projeto/Objeto 3/assets/js/principal.js
+++ Projeto/Objeto 3/assets/js/principal.js
@@ -1,23 +1,47 @@
-var loginVideo = document.querySelector('.scr-login video');
+let loginVideo = document.querySelector('#login-video');
+let loginConfirmacao = document.querySelector('#login-confirmacao');
+let buttonConfirmacao = document.querySelector("#button-confirmacao");
 
-var source = document.createElement('source');
+let source = document.createElement('source');
 source.setAttribute('src', 'assets/video/login-1.mp4');
 loginVideo.appendChild(source);
-// loginVideo.play()
+loginVideo.play();
 
-loginVideo.addEventListener('canplay', function() {
+let buttonClickado = false;
+let finalizadoZoom = false;
+let finalizado = false;
+
+buttonConfirmacao.addEventListener("click", function() {
+	if (!(loginConfirmacao.value === '')) {
+		buttonClickado = true;
+		setInterval(animBox, 800);
+		var nomePessoa = loginConfirmacao.value;
+	}
+});
+
+loginVideo.addEventListener("ended", function() {
+
+	if (!finalizado && buttonClickado) {
+		if (!finalizadoZoom) {
+			finalizadoZoom = true;
+			source.setAttribute('src', 'assets/video/login-2.mp4');
+		} else {
+			finalizado = true;
+			source.setAttribute('src', 'assets/video/login-3.mp4');
+		}
+		loginVideo.load();
+	}
+
 	loginVideo.play();
 });
 
-let loginButtonConfirmacao = document.querySelector("#login-button-confirmacao");
-console.log(loginButtonConfirmacao);
-loginButtonConfirmacao.addEventListener('click', function() {
-	loginVideo.addEventListener('ended', function myHandler(e) {
-
-		var source = document.createElement('source');
-		source.setAttribute('src', 'assets/video/login-2.mp4');
-		loginVideo.appendChild(source);
-		loginVideo.play();
-
-	}, false);
-});
+let animAux = 1.0;
+function animBox() {
+	if(animAux == 0){
+		clearInterval();
+	} else {
+		animAux -= 0.1;
+		loginConfirmacao.style.opacity = animAux;
+		buttonConfirmacao.style.opacity = animAux;
+	}
+}
