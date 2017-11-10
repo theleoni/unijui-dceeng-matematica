3dabe35400112b28c10ae08548178186fd7b3dfd
Guilherme Leoni <guilherme_leoni@outlook.com>
2017-11-10 15:17:46 -0200

    [Objeto 3] Adição da raiz quadrada na calculadora

--- Projeto/Objeto 3/assets/js/calculadora.js
+++ Projeto/Objeto 3/assets/js/calculadora.js
@@ -1,7 +1,7 @@
 function carregarCalculadora() {
 	// Get all the keys from document
 	var keys = document.querySelectorAll('#calculadora span');
-	var operators = ['+', '-', 'x', '÷'];
+	var operators = ['+', '-', 'x', '÷', '√'];
 	var decimalAdded = false;
 
 	// Add onclick event to all the keys and perform operations
@@ -15,7 +15,14 @@ function carregarCalculadora() {
 			// Now, just append the key values (btnValue) to the input string and finally use javascript's eval function to get the result
 			// If clear key is pressed, erase everything
 			if(btnVal == 'C') {
-				input.innerHTML = '';
+
+				if (input.innerHTML.length > 1) {
+
+					input.innerHTML = input.innerHTML.substring(0, input.innerHTML.length - 1);
+				} else {
+
+					input.innerHTML = "";
+				}
 				decimalAdded = false;
 			}
 
@@ -25,7 +32,10 @@ function carregarCalculadora() {
 				var lastChar = equation[equation.length - 1];
 
 				// Replace all instances of x and ÷ with * and / respectively. This can be done easily using regex and the 'g' tag which will replace all instances of the matched character/substring
-				equation = equation.replace(/x/g, '*').replace(/÷/g, '/');
+				equation = equation.replace(/x/g, '*').replace(/÷/g, '/').replace(/√/g, 'Math.sqrt(');
+				if (equation.includes('Math.sqrt')) {
+					equation += ')';
+				}
 
 				// Final thing left to do is checking the last character of the equation. If it's an operator or a decimal, remove it
 				if(operators.indexOf(lastChar) > -1 || lastChar == '.')
@@ -55,7 +65,7 @@ function carregarCalculadora() {
 				input.innerHTML += btnVal;
 
 				// Allow minus if the string is empty
-				else if(inputVal == '' && btnVal == '-')
+				else if(inputVal == '' && (btnVal == '-' || btnVal == '√'))
 				input.innerHTML += btnVal;
 
 				// Replace the last operator (if exists) with the newly pressed operator
