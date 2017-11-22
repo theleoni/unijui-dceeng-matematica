c96a7297b1c000795595f4199d635eda7b858653
Antonio Carlos Catani Neto <cataninetoo@gmail.com>
2017-11-22 13:33:44 -0200

    [Objeto 2] Correções impressas

--- Projeto/assets/js/Objeto 2/dataObj2.js
+++ Projeto/assets/js/Objeto 2/dataObj2.js
@@ -1,11 +1,11 @@
 	var dados =  {
 		"telaNome": {
 			"falaChico1": "Esses adolescentes de hoje em dia só querem saber de tecnologia, esquecem até de comer.",
-			"falaChico2": "Ei, vocês sabem o que esses aparelhos eletrônicos tem em comum?",
+			"falaChico2": "Ei, vocês sabem o que esses aparelhos eletrônicos têm em comum?",
 			"falaCrianças1": "Não, o que senhor Chico?",
 			"falaChico3": "Joana! Todos funcionam através de uma energia.",
 			"falaCrianças2": "Mas como é gerada a energia?",
-			"falaChico4": "Mateus! Existem várias formas de gerar energia, como por exemplo eólica, solar, biomassa, hidrelétrica, entre outras.",
+			"falaChico4": "Mateus! Existem várias formas de gerar energia como por exemplo eólica, solar, biomassa, hidrelétrica, entre outras.",
 			"falaCrianças3": "Pai! Como a água pode gerar energia?",
 			"falaChico5": "Arthur! Veja se encontra a resposta usando o seu notebook!",
 			"falaCrianças4": "Encontrei um vídeo, vamos assistir?"
@@ -23,29 +23,29 @@
 			"falaCrianças4": "Mas seu Chico, primeiro precisamos de um tema para tirar os dados utilizados nos cálculos.",
 			"falaChico6": "Que tal usarem o assunto que acabamos de conversar? No site da ANEEL vocês podem encontrar informações de todo o Brasil.",
 			"falaCrianças5": "É uma ótima ideia pai, vou ver o que encontro lá.",
-			"falaCrianças6": "Vamos estudar o conteúdo de estatística, vou começar pela média aritmética."
+			"falaCrianças6": "Vamos estudar o conteúdo de Estatística, vou começar pela média aritmética."
 		},
 
 		"telaPrimeirasQuestoes": {
 			"tituloQuestao1": "Analise as alternativas à seguir, considerando as informações apresentadas na tabela assinale a alternativa correta",
-			"afirmacao1Questao1": "%verdadeiro%<b>%numeroAlternativa%</b> - A média de consumo de energia elétrica por consumidor residencial no mês de Janeiro de 2017 é obtida pela divisão do consumo total mensal pelo número de consumidores residenciais do mês.",
+			"afirmacao1Questao1": "%verdadeiro%<b>%numeroAlternativa%</b> - A média de consumo de energia elétrica por consumidor residencial no mês de Janeiro de 2017, é obtida pela divisão do consumo total mensal pelo número de consumidores residenciais do mês.",
 			"afirmacao2Questao1": "%verdadeiro%<b>%numeroAlternativa%</b> - A média de consumo de energia elétrica por consumidor residencial, no Brasil, em Janeiro de 2017 é 172,33kWh.",
-			"afirmacao3Questao1": "<b>%numeroAlternativa%</b> - A média de consumo de energia elétrica por consumidor residencial no mês de Janeiro de 2017, na região Sudeste foi de 172,33kWh, visto que, esta é a média nacional.",
-			"afirmacao4Questao1": "%verdadeiro%<b>%numeroAlternativa%</b> - A média de consumo de energia elétrica por consumidor residencial no mês de Janeiro de 2017, na região nordeste foi de 122,77kWh, visto que este valor é obtido pelo total de energia gasta nesta região, dividido pelo número de consumidores residenciais.",
-			"afirmacao5Questao1": "<b>%numeroAlternativa%</b> - A média de consumo de energia elétrica por consumidor residencial no mês de Janeiro de 2017, na região Sul foi de 204,90kWh, visto que este é o valor da média nacional.",
+			"afirmacao3Questao1": "<b>%numeroAlternativa%</b> - A média de consumo de energia elétrica por consumidor residencial no mês de Janeiro de 2017, na região Sudeste, foi de 172,33kWh, visto que, esta é a média nacional.",
+			"afirmacao4Questao1": "%verdadeiro%<b>%numeroAlternativa%</b> - A média de consumo de energia elétrica por consumidor residencial no mês de Janeiro de 2017, na região nordeste, foi de 122,77kWh, visto que este valor é obtido pelo total de energia gasta nesta região, dividido pelo número de consumidores residenciais.",
+			"afirmacao5Questao1": "<b>%numeroAlternativa%</b> - A média de consumo de energia elétrica por consumidor residencial no mês de Janeiro de 2017, na região Sul, foi de 204,90kWh, visto que este é o valor da média nacional.",
 			"alertaErrado": "<center><strong>Oops!</strong> Resposta incorreta.</center>",
 			"alertaCerto": "<center><strong>Parabéns!</strong> Resposta correta.</center>"
 		},
 		"questao2": {
-			"alternativa1": "A média de consumo no mês foi de 3,821kWh",
-			"alternativa2": "A média de consumo no mês foi de 14,3kWh",
-			"alternativa3": "A média de consumo no mês foi de 3,575kWh",
-			"alternativa4": "A média de consumo no mês foi de 0,511kWh",
-			"alternativa5": "A média de consumo no mês foi de 7,483kWh"
+			"alternativa1": "A média de consumo no mês foi de 3,821kWh.",
+			"alternativa2": "A média de consumo no mês foi de 14,3kWh.",
+			"alternativa3": "A média de consumo no mês foi de 3,575kWh.",
+			"alternativa4": "A média de consumo no mês foi de 0,511kWh.",
+			"alternativa5": "A média de consumo no mês foi de 7,483kWh."
 		},
 
 		"telaConversa3": {
-			"falaJoana1": "Já aprendemos a média, a mediana, e a moda. Percebemos também que é possível desenvolver esses cálculos de estatística usando dados de energia.",
+			"falaJoana1": "Já aprendemos a média, a mediana, e a moda. Percebemos também que é possível desenvolver esses cálculos de Estatística usando dados de energia.",
 			"falaMateus1": "Chegou a hora de fazer o nosso trabalho!",
 			"falaArthur1": "Contamos com a sua ajuda %fulano%."
 		},
@@ -54,7 +54,7 @@
 			"atv1": "<h1>Atividade 1</h1><br>Classifique a moda dos dados apresentados na tabela.",
 			"atv2": "<h1>Atividade 1</h1><br>Calcule e preencha a tabela com os valores da mediana.",
 			"atv3": "<h1>Atividade 1</h1><br>Organize a tabela em ordem crescente, de acordo com a mediana<br><h5>Para organizar a tabela clique a arraste a mesma</h5>",
-			"finalizada": "<h1>Atividade 1</h1><br>Parabéns! Você finalizou esta atividade. Retorne para a tela de seleção para realizar as demais atividades"
+			"finalizada": "<h1>Atividade 1</h1><br>Parabéns! Você finalizou esta atividade. Retorne para a tela de seleção para realizar as demais atividades."
 		},
 
 		"atividadeLivro1": {
@@ -77,22 +77,22 @@
 
 		},
 		"graficos": {
-			"gasNatural": "A linha verde indica a média no valor de 60284,2GWh (Gigawatt-hora), ou seja, se todos os valores fossem iguais seriam esse mesmo valor da média. A linha laranja indica a mediana, sendo assim o valor central dos dados, ou seja, 50% dos dados são iguais ou maiores que 69003GWh e 50% dos dados são iguais ou menores que 69003GWh.",
+			"gasNatural": "A linha verde indica a média no valor de 60284,2GWh (Gigawatt-hora), ou seja, se todos os valores fossem iguais teriam esse mesmo valor da média. A linha laranja indica a mediana, sendo assim o valor central dos dados, ou seja, 50% dos dados são iguais ou maiores que 69003GWh e 50% dos dados são iguais ou menores que 69003GWh.",
 			"hidreletrica": "A linha verde indica a média no vlaor de 393569,8GWh (Gigawatt-hora), ou seja, se todos os valores fossem iguais teriam esse mesmo valor da média. A linha laranja indica a mediana, sendo assim o valor central dos dados, ou seja, 50% dos dados são iguais ou maiores que 390992GWh e 50% dos dados são iguais ou menores que 390992GWh.",
-			"petroleo": "A linha verde indica a média no valor de 21546,8GWh (Gigawatt-hora), ou seja, se todos os valores fossem iguais seriam esse mesmo valor da média. A linha laranja indica a mediana, sendo assim o valor central dos dados, ou seja, 50% dos dados são iguais ou maiores que 22090GWh e 50% dos dados são iguais ou menores que 22090GWh.",
-			"carvao": "A linha verde indica a média no valor de 13437,8GWh (Gigawatt-hora), ou seja, se todos os valores fossem iguais seriam esse mesmo valor da média. A linha laranja indica a mediana, sendo assim o valor central dos dados, ou seja, 50% dos dados são iguais ou maiores que 14801GWh e 50% dos dados são iguais ou menores que 14801GWh.",
-			"nuclear": "A linha verde indica a média no valor de 15493,6GWh (Gigawatt-hora), ou seja, se todos os valores fossem iguais seriam esse mesmo valor da média. A linha laranja indica a mediana, sendo assim o valor central dos dados, ou seja, 50% dos dados são iguais ou maiores que 15659GWh e 50% dos dados são iguais ou menores que 15659GWh.",
-			"biomassa": "A linha verde indica a média no valor de 39671GWh (Gigawatt-hora), ou seja, se todos os valores fossem iguais seriam esse mesmo valor da média. A linha laranja indica a mediana, sendo assim o valor central dos dados, ou seja, 50% dos dados são iguais ou maiores que 39679GWh e 50% dos dados são iguais ou menores que 39679GWh.",
-			"eolica": "A linha verde indica a média no valor de 9633,8GWh (Gigawatt-hora), ou seja, se todos os valores fossem iguais seriam esse mesmo valor da média. A linha laranja indica a mediana, sendo assim o valor central dos dados, ou seja, 50% dos dados são iguais ou maiores que 6578GWh e 50% dos dados são iguais ou menores que 6578GWh.",
-			"outras": "A linha verde indica a média no valor de 11838,2GWh (Gigawatt-hora), ou seja, se todos os valores fossem iguais seriam esse mesmo valor da média. A linha laranja indica a mediana, sendo assim o valor central dos dados, ou seja, 50% dos dados são iguais ou maiores que 12241GWh e 50% dos dados são iguais ou menores que 12241GWh."
+			"petroleo": "A linha verde indica a média no valor de 21546,8GWh (Gigawatt-hora), ou seja, se todos os valores fossem iguais teriam esse mesmo valor da média. A linha laranja indica a mediana, sendo assim o valor central dos dados, ou seja, 50% dos dados são iguais ou maiores que 22090GWh e 50% dos dados são iguais ou menores que 22090GWh.",
+			"carvao": "A linha verde indica a média no valor de 13437,8GWh (Gigawatt-hora), ou seja, se todos os valores fossem iguais teriam esse mesmo valor da média. A linha laranja indica a mediana, sendo assim o valor central dos dados, ou seja, 50% dos dados são iguais ou maiores que 14801GWh e 50% dos dados são iguais ou menores que 14801GWh.",
+			"nuclear": "A linha verde indica a média no valor de 15493,6GWh (Gigawatt-hora), ou seja, se todos os valores fossem iguais teriam esse mesmo valor da média. A linha laranja indica a mediana, sendo assim o valor central dos dados, ou seja, 50% dos dados são iguais ou maiores que 15659GWh e 50% dos dados são iguais ou menores que 15659GWh.",
+			"biomassa": "A linha verde indica a média no valor de 39671GWh (Gigawatt-hora), ou seja, se todos os valores fossem iguais teriam esse mesmo valor da média. A linha laranja indica a mediana, sendo assim o valor central dos dados, ou seja, 50% dos dados são iguais ou maiores que 39679GWh e 50% dos dados são iguais ou menores que 39679GWh.",
+			"eolica": "A linha verde indica a média no valor de 9633,8GWh (Gigawatt-hora), ou seja, se todos os valores fossem iguais teriam esse mesmo valor da média. A linha laranja indica a mediana, sendo assim o valor central dos dados, ou seja, 50% dos dados são iguais ou maiores que 6578GWh e 50% dos dados são iguais ou menores que 6578GWh.",
+			"outras": "A linha verde indica a média no valor de 11838,2GWh (Gigawatt-hora), ou seja, se todos os valores fossem iguais teriam esse mesmo valor da média. A linha laranja indica a mediana, sendo assim o valor central dos dados, ou seja, 50% dos dados são iguais ou maiores que 12241GWh e 50% dos dados são iguais ou menores que 12241GWh."
 		},
 
 		"telaAtividade3": {
 			"atv1": "<h1>Atividade 3</h1><br>Preencha os valores da média e mediana de acordo com os dados apresentados na tabela.",
 			"atv2": "<h1>Atividade 3</h1><br>Em quais regiões a mediana é superior à média?",
 			"atv3": "<h1>Atividade 3</h1><br>Em quais regiões a média é superior à mediana?",
-			"atv4": "<h1>Atividade 3</h1><br>Calcule a diferença entre a média e mediana para cada uma das regiões (Média - Mediana)",
-			"finalizada": "Parabéns! Você finalizou esta atividade. Retorne para a tela de seleção para realizar as demais atividades"
+			"atv4": "<h1>Atividade 3</h1><br>Calcule a diferença entre a média e mediana para cada uma das regiões (Média - Mediana).",
+			"finalizada": "Parabéns! Você finalizou esta atividade. Retorne para a tela de seleção para realizar as demais atividades."
 
 		},
 		"atividadeLivro3": {
@@ -104,7 +104,7 @@
 		},
 		"telaAtividade4": {
 			"atv1": "<h1>Atividade 4</h1><br>Calcule a moda e a mediana do percentual de aumento utilizando os seguintes dados.",
-			"finalizada": "<h1>Atividade 4</h1><br>Parabéns! Você finalizou esta atividade. Retorne para a tela de seleção para realizar as demais atividades"
+			"finalizada": "<h1>Atividade 4</h1><br>Parabéns! Você finalizou esta atividade. Retorne para a tela de seleção para realizar as demais atividades."
 
 		},
 		"atividadeLivro4": {
